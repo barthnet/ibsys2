53655b90238f76776f02a52d5e31d75739a49416
Daniel <Wrner>
2013-05-10 13:24:30 +0200

    Attribut zur Speicherung von Bearbeitungsplatz, Dauer und Ruestzeit hinzugefügt

--- app/models/ProducedPart.java
+++ app/models/ProducedPart.java
@@ -1,9 +1,24 @@
 package models;
 
+import java.util.HashMap;
+
 import javax.persistence.Entity;
 
+/**
+ * A class for the self produced parts.
+ * 
+ * @extends {@link AbstractPart}
+ * @author DaNieKL
+ * 
+ */
 @Entity
 public class ProducedPart extends AbstractPart {
-
 	
+	/**
+	 * Bearbeitungszeiten und Rüstzeiten.
+	 * Die erste Map dient dazu, den Bearbeitungsplatz anhand der Nummer abzuspeichern und das Array
+	 * speichert Bearbeitungsdauer und Rüstzeit. 
+	 */
+	private HashMap<Integer, int[][]> mDuration;
+
 }
