9d2ef7ae6109a07664ae1c879f6bd3994fc2adb9
Daniel <Wrner>
2013-05-10 12:26:24 +0200

    updated Attributes for AbstracPart

--- app/models/AbstractPart.java
+++ app/models/AbstractPart.java
@@ -2,30 +2,132 @@ package models;
 
 import play.db.jpa.Model;
 
-public abstract class AbstractPart extends Model{
+/**
+ * Abstrakte Klasse fuer einen Artikel
+ *
+ * @author Woda
+ *
+ */
+public abstract class AbstractPart extends Model {
 
+	/**
+	 * Artikelnummer
+	 */
 	private Long id;
-	private String name;
-	private double value;
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public void setValue(double value) {
-		this.value = value;
-	}
+
+	/**
+	 * Lagerbestand
+	 */
+	private int amount;
+
+	/**
+	 * Lagerwert
+	 */
+	private double stockvalue;
+
+	/**
+	 * Startmenge
+	 */
+	private int startamount;
+
+	/**
+	 * Menge/Startmenge
+	 */
+	private double pct;
+
+	/**
+	 * Preis
+	 */
+	private double price;
+
+	/**
+	 * @return the id
+	 */
 	public Long getId() {
 		return id;
 	}
-	public String getName() {
-		return name;
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
 	}
-	public double getValue() {
-		return value;
+
+	/**
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
 	}
-	
-	
+
+	/**
+	 * @param amount
+	 *            the amount to set
+	 */
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	/**
+	 * @return the stockvalue
+	 */
+	public double getStockvalue() {
+		return stockvalue;
+	}
+
+	/**
+	 * @param stockvalue
+	 *            the stockvalue to set
+	 */
+	public void setStockvalue(double stockvalue) {
+		this.stockvalue = stockvalue;
+	}
+
+	/**
+	 * @return the startamount
+	 */
+	public int getStartamount() {
+		return startamount;
+	}
+
+	/**
+	 * @param startamount
+	 *            the startamount to set
+	 */
+	public void setStartamount(int startamount) {
+		this.startamount = startamount;
+	}
+
+	/**
+	 * @return the pct
+	 */
+	public double getPct() {
+		return pct;
+	}
+
+	/**
+	 * @param pct
+	 *            the pct to set
+	 */
+	public void setPct(double pct) {
+		this.pct = pct;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price
+	 *            the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
