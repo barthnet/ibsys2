2d9e824d792ff63098a316e4e33640cbb49ea5b9
mopa <patrickmos@googlemail.com>
2013-05-11 12:37:52 +0200

    add period date model

---
+++ app/models/PeriodDate.java
@@ -0,0 +1,52 @@
+package models;
+
+/**
+ * 
+ * @author mopa
+ *
+ */
+public class PeriodDate {
+	
+	private int period;
+	
+	private int day;
+	
+	private int hour;
+	
+	private int minute;
+
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+	
+	
+
+}
