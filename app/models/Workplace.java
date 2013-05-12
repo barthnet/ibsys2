1f53ec79d62c4a47fd2cd0bb724d01192be8aa27
Daniel <Wrner>
2013-05-13 00:59:31 +0200

    neue (unfertige) Klasse für Arbeitsplätze

---
+++ app/models/Workplace.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * @author Woda
+ *
+ */
+public class Workplace {
+
+	private int Id;
+	private int idletime;
+	private double machineIdleTimeCosts;
+	private int setupEvents;
+	private double wageCosts;
+	private double wageIdleTimeCosts;
+	private int timeNeed;
+		
+}
