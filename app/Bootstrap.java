18b311011829200531f0302ad98f8d9d223b2837
svnmrc <ryoko@public-files.de>
2013-05-31 12:48:18 +0200

    initial-productionPlan.yml loaded in Bootstrap

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -32,7 +32,7 @@ public class Bootstrap extends Job {
 	public void doJob() {
 		Logger.info("Bootstrap");
 		Fixtures.deleteAllModels();
-		Fixtures.loadModels("initial-items.yml", "initial-distributions.yml", "initial-workplaces.yml", "ItemTime.yml", "initial-dispositionOrder.yml");
+		Fixtures.loadModels("initial-items.yml", "initial-distributions.yml", "initial-workplaces.yml", "ItemTime.yml", "initial-dispositionOrder.yml", "initial-productionPlan.yml");
 		createComponents();
 	}
 
