00503e70d813c9d2a30bb83e88574b7519167b5a
Woda1011 <woerner.daniel@gmail.com>
2013-05-31 15:55:04 +0200

    merge Method for merging the new attributes in the old one

--- app/models/DistributionWish.java
+++ app/models/DistributionWish.java
@@ -15,8 +15,25 @@ public class DistributionWish extends Model {
 	public int period1;
 	public int period2;
 	public int period3;
+
 	@Override
 	public String toString() {
-		return "DistributionWish [item=" + item + ", period=" + period + ", period1=" + period1 + ", period2=" + period2 + ", period3=" + period3 + "]";
-	}	
+		return "DistributionWish [item=" + item + ", period=" + period + ", period1=" + period1
+				+ ", period2=" + period2 + ", period3=" + period3 + "]";
+	}
+
+	public void merge(DistributionWish other) {
+
+		this.period = other.period;
+		this.period1 = other.period1;
+		this.period2 = other.period2;
+		this.period3 = other.period3;
+
+		this.item.name = other.item.name;
+		this.item.name_en = other.item.name_en;
+		this.item.price = other.item.price;
+		this.item.type = other.item.type;
+		this.save();
+	}
+
 }
