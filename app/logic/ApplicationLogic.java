bd9510bf71bc451c2697d2e2763205bc7a31f28a
Woda1011 <woerner.daniel@gmail.com>
2013-06-03 11:56:46 +0200

    added a method to map the production orders to the workplaces

--- app/logic/ApplicationLogic.java
+++ app/logic/ApplicationLogic.java
@@ -1,5 +1,29 @@
 package logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import utils.ItemHelper;
+
+import models.ProductionOrder;
+import models.Workplace;
+
 public class ApplicationLogic {
 
+	public static void mapProductionOrders(List<ProductionOrder> orders) {
+
+		List<Workplace> workplaces = new ArrayList<Workplace>();
+
+		// Für alle ProductionOrders...
+		for (ProductionOrder order : orders) {
+
+			// Gib mir für das Item der Order alle Workplaces
+			workplaces = ItemHelper.getWorkplaces(order.item);
+
+			// Füge jedem dieser Workplaces die Order hinzu
+			for (Workplace workplace : workplaces) {
+				workplace.productionPlanList.add(order);
+			}
+		}
+	}
 }
