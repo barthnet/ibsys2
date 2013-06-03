f203dab4ce353322241438b8556be083876af53c
Woda1011 <woerner.daniel@gmail.com>
2013-06-03 12:45:25 +0200

    new method for calculating the capacity

--- app/logic/ApplicationLogic.java
+++ app/logic/ApplicationLogic.java
@@ -3,10 +3,11 @@ package logic;
 import java.util.ArrayList;
 import java.util.List;
 
-import utils.ItemHelper;
-
+import models.Capacity;
 import models.ProductionOrder;
+import models.WaitingList;
 import models.Workplace;
+import utils.ItemHelper;
 
 public class ApplicationLogic {
 
@@ -24,6 +25,60 @@ public class ApplicationLogic {
 			for (Workplace workplace : workplaces) {
 				workplace.productionPlanList.add(order);
 			}
+		}
+	}
+
+	public static void calcCapacity() {
+		List<Workplace> workplaces = Workplace.findAll();
+
+		for (Workplace workplace : workplaces) {
+			Capacity capa = new Capacity();
+
+			// Warteliste dazurechnen
+			for (WaitingList wait : workplace.waitingList) {
+
+				int duration = ItemHelper.getProcessTime(workplace, wait.item);
+				int setup = ItemHelper.getSetupTime(workplace, wait.item);
+
+				capa.time += duration * wait.amount;
+				capa.setupTime += setup;
+			}
+			// ProcessList dazurechnen
+			capa.time += ItemHelper.getProcessTime(workplace, workplace.processList.item);
+			capa.time += ItemHelper.getSetupTime(workplace, workplace.processList.item);
+			
+			//ProductionPlanList dazurechnen
+			for(ProductionOrder order :workplace.productionPlanList) {
+				
+				int duration = ItemHelper.getProcessTime(workplace, order.item);
+				int setup = ItemHelper.getSetupTime(workplace, order.item);
+				
+				capa.time += duration * order.amount;
+				capa.setupTime += setup;				
+			}
+			
+			// Gesamtdauer berechnen
+			capa.totaltime = capa.time + capa.setupTime;
+					
+			// Überstunden berechnen
+			// Schichten berechnen
+			if(capa.totaltime < 2400) {
+				capa.shift = 1;
+				capa.overtime = capa.totaltime - 2400;
+			}
+			
+			if(capa.totaltime < 3600) {
+				capa.shift = 2;
+				capa.overtime = capa.totaltime - 3600;
+			}
+			
+			if(capa.totaltime < 4800) {
+				capa.shift = 3;
+				capa.overtime = capa.totaltime - 4800;
+			}
+						
+			//Capacity Objekt persistieren
+			capa.save();
 		}
 	}
 }
